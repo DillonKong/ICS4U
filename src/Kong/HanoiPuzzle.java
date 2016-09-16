bb5b5f18adee5fdf32b18ae5fdf14f132fc814f2
Owner <Owner@192.168.0.15>
2016-09-16 14:57:02 -0400

    HanoiPuzzle

---
+++ src/Kong/HanoiPuzzle.java
@@ -0,0 +1,14 @@
+package Kong;
+
+import java.util.Scanner;
+
+public class HanoiPuzzle {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int disc = scan.nextInt();
+		
+	}
+	public static 
+
+}
