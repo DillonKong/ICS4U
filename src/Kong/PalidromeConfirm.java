cca1ef3ac3a13d6fba423697be4c572985637d73
Owner <Owner@192.168.0.15>
2016-09-15 20:42:07 -0400

    Palindrome extra

--- src/Kong/PalidromeConfirm.java
+++ src/Kong/PalidromeConfirm.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class PalidromeConfirm {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner (System.in);
 		String one = scan.nextLine(), two = scan.nextLine();
+		String wordBackwards = isReverse(one);
 		
-
+		if (wordBackwards.equalsIgnoreCase(two))
+			System.out.println("True");
+		else
+			System.out.println("False");
+		
 	}
-	public static String isReverse (String x, String y)
+	public static String isReverse (String x)
 	{
-		int x = 0;
-		
+		char backwards = x.charAt(x.length()-1);
+        if(x.length() == 1) 
+        	return Character.toString(backwards);   
+
+        return backwards + isReverse(x.substring(0,x.length()-1));
 	}
 
 }
