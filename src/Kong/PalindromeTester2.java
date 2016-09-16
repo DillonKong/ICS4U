ada8590c16c07f30960665b590a8a77c69d2c359
Owner <Owner@192.168.0.15>
2016-09-15 21:00:48 -0400

    Modified PalindromeTester

---
+++ src/Kong/PalindromeTester2.java
@@ -0,0 +1,61 @@
+package Kong;
+
+import java.util.Scanner;
+/*Dillon Kong
+ * PalindromeTester2
+ * User inputs possible palindrome and program determines whether it is or not.
+ * 15/9/16
+ */
+public class PalindromeTester2 {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner (System.in);
+		boolean  again = true;
+
+		while(again == true)
+		{
+			System.out.println("Please input potential palindrome.");
+
+			String one = scan.nextLine();
+			String another;
+			String wordBackwards;
+
+			wordBackwards = isReverse(one);
+
+			if (wordBackwards.equalsIgnoreCase(one))
+			{
+				System.out.println("This word IS a possible palindrome.\n Test another palindrome. y/n?");
+				another = scan.nextLine();
+				if (another.equalsIgnoreCase("y"))
+				{
+					again = true;
+				}
+				else if (another.equalsIgnoreCase("n"))
+					System.exit(0);
+			}
+			else
+			{
+				System.out.println("This IS Not a possible palindrome \n Test another palindrome. y/n?");
+				another = scan.nextLine();
+				if (another.equalsIgnoreCase("y"))
+				{
+					again = true;
+				}
+				else if (another.equalsIgnoreCase("n"))
+					System.exit(0);
+			}
+		}
+	}
+	
+	public static String isReverse (String x)
+	{
+		char backwards = x.charAt(x.length()-1);
+		
+		if(x.length() == 1) 
+			return Character.toString(backwards);   
+
+		return backwards + isReverse(x.substring(0,x.length()-1));
+	}
+
+}
